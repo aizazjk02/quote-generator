18191f3d017d18bdc2b29cd1fed4b02228ca2252
Aizaz Kotwal <aizazk0309@protonmail.com>
2023-12-14 01:52:28 +0530

    add support  for vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { ToastContainer } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css";
 
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from '@vercel/analytics/react';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
     <App />
     <ToastContainer />
     <SpeedInsights />
+    <Analytics />
   </>,
 )
