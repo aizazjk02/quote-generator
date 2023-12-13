06faf3e45018a0b9be8a103580a9f3ddab8e8e36
Aizaz Kotwal <aizazk0309@protonmail.com>
2023-12-14 01:50:01 +0530

    add support for vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ToastContainer } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css";
+
+import { SpeedInsights } from "@vercel/speed-insights/react"
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
     <App />
     <ToastContainer />
+    <SpeedInsights />
   </>,
 )
